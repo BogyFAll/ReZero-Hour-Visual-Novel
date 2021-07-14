737ff20c5f114b7cc68b0591376c90f7f4bbe05d
BogyFAll <bogyfall@gmail.com>
2021-07-14 22:10:47 +0300

    Доделал визуализацию звука

--- Assets/Scripts/Visualizer.cs
+++ Assets/Scripts/Visualizer.cs
@@ -15,14 +15,24 @@ namespace VisualNovel.UI
 
 		[SerializeField] private List<Image> _images = new List<Image>();
 
+		private void Start()
+		{
+			_images.ForEach(e => e.enabled = true);
+		}
+
 		private void Update()
 		{
 			if(_isVisualizer)
 			{
 				_audioSource.GetSpectrumData( _samples, 0, _type );
 
-				for ( int i = 0; i < _samples.Length; i++ )
+				for ( int i = 0; i < 16; i++ )
+				{
 					_images[i].fillAmount = _samples[i];
+					_images[i + 16].fillAmount = _samples[i];
+					_images[i + 32].fillAmount = _samples[i];
+					_images[i + 48].fillAmount = _samples[i];
+				}
 			}
 		}
 	}
